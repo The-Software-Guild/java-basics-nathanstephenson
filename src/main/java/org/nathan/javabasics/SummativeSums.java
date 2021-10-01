aa0617211566b6c906d442bba45ec28a39b7c5f3
nathanstephenson <nathans261@gmail.com>
2021-10-01 13:21:16 +0100

    004: SummativeSums (using a user input as the array of ints)

---
+++ src/main/java/org/nathan/javabasics/SummativeSums.java
@@ -0,0 +1,40 @@
+package org.nathan.javabasics;
+
+import java.util.Scanner;
+
+public class SummativeSums {
+    public static void main(String[] args) {//doesn't pick up the last value
+        Scanner scanner = new Scanner(System.in);
+        String valuesInput;
+        int sum = 0;
+        int valueCount = 0;
+
+        System.out.println("Please enter a list of (integer) values, separated by commas, to sum:");
+        valuesInput = scanner.nextLine();
+        valuesInput += ',';
+        char[] valuesCharArray = valuesInput.toCharArray();
+        for(char c : valuesCharArray){
+            if(c == ','){
+                valueCount++;
+            }
+        }
+        int[] valueArray = new int[valueCount];
+        String valueString = "";
+        int arrayPos = 0;
+        for(int i = 0; i < valuesCharArray.length; i++){
+            if(valuesCharArray[i] == ',' || (i == valuesCharArray.length - 1 && arrayPos == valueCount - 1)){
+                if(!valueString.strip().isEmpty()){
+                    valueArray[arrayPos] = Integer.parseInt(valueString.strip());
+                }
+                valueString = "";
+                arrayPos++;
+            }else{
+                valueString += valuesCharArray[i];
+            }
+        }
+        for (int n : valueArray){
+            sum += n;
+        }
+        System.out.println("Total sum of these " + valueCount + " values is: " + sum);
+    }
+}
